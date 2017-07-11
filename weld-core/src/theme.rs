45b83851c6818cae2548f2bb6147b9dfe66e8a8b
Roy Jacobs <roy.jacobs@gmail.com>
2017-07-11 14:49:18 +0200

    More cleanup

--- weld-core/src/theme.rs
+++ weld-core/src/theme.rs
@@ -57,28 +57,43 @@ impl<'a> VisualBuilder<'a> {
         let node = self.logical_tree.get(logical_node_id).unwrap();
         println!("Adding {:?}", node.data().component_type);
 
-        let mut visual_node = Node::new(Visual { yoga: YogaNode::new() });
-        let mut visual_node_id = None;
+        let mut yoga = YogaNode::new();
+        yoga.apply_styles(&node.data().styles);
+        let visual_node = Node::new(Visual { yoga });
 
-        if let Some(parent_id) = node.parent() {
-            let visual_parent_id = self.node_to_visual_map.get(&self.logical_tree.get(parent_id).unwrap().data().id).unwrap();
-            {
-                let mut visual = visual_node.data_mut();
-                let mut parent_yoga = &mut self.visual_tree.get_mut(visual_parent_id).unwrap().data_mut().yoga;
-                let child_count = parent_yoga.child_count();
-                parent_yoga.insert_child(&mut visual.yoga, child_count);
-            }
-
-            visual_node_id = Some(self.visual_tree.insert(visual_node, InsertBehavior::UnderNode(visual_parent_id)).unwrap());
-        } else {
-            visual_node_id = Some(self.visual_tree.insert(visual_node, InsertBehavior::AsRoot).unwrap());
-        }
-
-        self.node_to_visual_map.insert(node.data().id, visual_node_id.unwrap());
+        let visual_parent_id = self.get_visual_node(node.parent());
+        self.insert_visual_node(node, visual_node, visual_parent_id);
 
         for child_id in node.children() {
             self.add_node(child_id);
         }
+    }
+
+    fn get_visual_node(&self, logical_parent_node: Option<&NodeId>) -> Option<NodeId> {
+        match logical_parent_node {
+            Some(logical_parent_id) => {
+                let key = &self.logical_tree.get(logical_parent_id).unwrap().data().id;
+                Some(self.node_to_visual_map.get(key).unwrap().clone())
+            }
+            None => None
+        }
+    }
+
+    fn insert_visual_node(&mut self, logical_node: &Node<Box<Component>>, mut visual_node: Node<Visual>, visual_parent_id: Option<NodeId>) {
+        // The new node has a visual parent, so attach the YogaNode to the parent YogaNode as well
+        if let Some(ref vpi) = visual_parent_id {
+            let mut visual = visual_node.data_mut();
+            let mut parent_yoga = &mut self.visual_tree.get_mut(&vpi).unwrap().data_mut().yoga;
+            let child_count = parent_yoga.child_count();
+            parent_yoga.insert_child(&mut visual.yoga, child_count);
+        }
+
+        let insert_behavior = match visual_parent_id {
+            Some(ref vpi) => InsertBehavior::UnderNode(&vpi),
+            None => InsertBehavior::AsRoot
+        };
+        let visual_node_id = Some(self.visual_tree.insert(visual_node, insert_behavior).unwrap());
+        self.node_to_visual_map.insert(logical_node.data().id, visual_node_id.unwrap());
     }
 }
 
@@ -88,7 +103,7 @@ mod tests {
     use components;
     use components::panel::PanelSize;
     use component_tree::ComponentTree;
-    use yoga::{Direction, Layout, Percent};
+    use yoga::{Direction, Layout, Percent, FlexStyle};
 
     #[test]
     fn test_splitter() {
@@ -96,14 +111,22 @@ mod tests {
         let root = tree.add_node(components::SplitterBuilder::new(), None);
 
         let mut panel1: Component = components::PanelBuilder::new().into();
-        panel1.data_mut().put(PanelSize { size: (30.percent(), 100.percent()) });
+        //panel1.data_mut().put(PanelSize { size: (30.percent(), 100.percent()) });
+        panel1.styles = vec![
+            FlexStyle::Width(30.percent()),
+            FlexStyle::Height(100.percent())
+        ];
         tree.add_node(panel1, Some(&root));
 
         let mut panel2: Component = components::PanelBuilder::new().into();
-        panel2.data_mut().put(PanelSize { size: (70.percent(), 100.percent()) });
+        //panel2.data_mut().put(PanelSize { size: (70.percent(), 100.percent()) });
+        panel2.styles = vec![
+            FlexStyle::Width(70.percent()),
+            FlexStyle::Height(100.percent())
+        ];
         tree.add_node(panel2, Some(&root));
 
-        let mut visual_tree = VisualBuilder::new(tree.tree()).build_visual_tree();
+        let visual_tree = VisualBuilder::new(tree.tree()).build_visual_tree();
 
         let sizes: Vec<Layout> = visual_tree.traverse_pre_order(visual_tree.root_node_id().unwrap())
             .unwrap()
