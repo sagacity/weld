59b6a366b68fb6db0fdb21885b678969d3982773
Roy Jacobs <roy.jacobs@gmail.com>
2017-07-13 20:51:19 +0200

    Using regular Glutin instead of servo-glutin

--- weld-core/src/window.rs
+++ weld-core/src/window.rs
@@ -1,34 +1,35 @@
 use gleam::gl;
 use glutin;
+use glutin::GlContext;
 use webrender;
 use webrender::api::*;
 use component_tree::ComponentTree;
 use theme::Theme;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 struct Notifier {
-    window_proxy: glutin::WindowProxy,
+    proxy: glutin::EventsLoopProxy
 }
 
 impl Notifier {
-    fn new(window_proxy: glutin::WindowProxy) -> Notifier {
-        Notifier { window_proxy: window_proxy }
+    fn new(proxy: glutin::EventsLoopProxy) -> Notifier {
+        Notifier { proxy: proxy }
     }
 }
 
 impl RenderNotifier for Notifier {
     fn new_frame_ready(&mut self) {
         #[cfg(not(target_os = "android"))]
-        self.window_proxy.wakeup_event_loop();
+        let _ = self.proxy.wakeup().unwrap();
     }
 
     fn new_scroll_frame_ready(&mut self, _composite_needed: bool) {
         #[cfg(not(target_os = "android"))]
-        self.window_proxy.wakeup_event_loop();
+        let _ = self.proxy.wakeup().unwrap();
     }
 }
 
 pub trait Window {
-    fn size(&self) -> &DeviceUintSize;
     fn run(&mut self);
     fn update_tree(&mut self, tree_builder: &Fn() -> ComponentTree);
 }
@@ -36,141 +37,149 @@ pub trait Window {
 pub struct WindowFactory;
 
 struct WebrenderWindow {
-    window: glutin::Window,
-    renderer: webrender::Renderer,
-    api: RenderApi,
-    size: DeviceUintSize, 
-    tree: ComponentTree,
-    theme: Theme,
-    epoch: u32
+    title: &'static str,
+    tree: ComponentTree
 }
 
+struct TreeBuilderContext<'a> {
+    size: DeviceUintSize,
+    theme: &'a Theme,
+    api: &'a RenderApi,
+    epoch: &'a Epoch,
+}
+
+
 impl WindowFactory {
-    pub fn new(title: &str) -> Box<Window> {
+    pub fn new(title: &'static str) -> Box<Window> {
+        return
+            Box::new(WebrenderWindow {
+                title,
+                tree: ComponentTree::new()
+            });
+    }
+}
+
+impl Window for WebrenderWindow {
+    fn run(&mut self) {
+        let mut events_loop = glutin::EventsLoop::new();
         let window = glutin::WindowBuilder::new()
-            .with_title(title)
-            .with_multitouch()
-            .with_gl(glutin::GlRequest::GlThenGles {
-                opengl_version: (3, 2),
-                opengles_version: (3, 0),
-            })
-            .build()
-            .unwrap();
+            .with_title(self.title)
+            .with_multitouch();
+
+        let context = glutin::ContextBuilder::new().with_vsync(false);
+        let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
 
         unsafe {
-            window.make_current().ok();
-        }
+            let _ = gl_window.make_current().unwrap();
+        };
 
         let gl = match gl::GlType::default() {
             gl::GlType::Gl => unsafe {
-                gl::GlFns::load_with(|symbol| window.get_proc_address(symbol) as *const _)
+                gl::GlFns::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _)
             },
             gl::GlType::Gles => unsafe {
-                gl::GlesFns::load_with(|symbol| window.get_proc_address(symbol) as *const _)
-            },
+                gl::GlesFns::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _)
+            }
         };
 
         //println!("OpenGL version {}", gl.get_string(gl::VERSION));
         //println!("Shader resource path: {:?}", res_path);
 
-        let (width, height) = window.get_inner_size_pixels().unwrap();
+        let (width, height) = gl_window.get_inner_size_pixels().unwrap();
 
         let opts = webrender::RendererOptions {
             //resource_override_path: res_path,
             debug: true,
             precache_shaders: true,
-            device_pixel_ratio: window.hidpi_factor(),
+            device_pixel_ratio: 1.0,
+            //window.hidpi_factor(),
             ..webrender::RendererOptions::default()
         };
 
-        let size = DeviceUintSize::new(width, height);
-        let (renderer, sender) = webrender::renderer::Renderer::new(gl, opts, size).unwrap();
+        let (mut renderer, sender) = webrender::renderer::Renderer::new(gl, opts, DeviceUintSize::new(width, height)).unwrap();
         let api = sender.create_api();
 
         api.set_root_pipeline(PipelineId(0, 0));
 
-        let notifier = Box::new(Notifier::new(window.create_window_proxy()));
+        let notifier = Box::new(Notifier::new(events_loop.create_proxy()));
         renderer.set_render_notifier(notifier);
 
-        return Box::new(WebrenderWindow {
-            window: window,
-            renderer: renderer,
-            api: api,
-            size: size, 
-            tree: ComponentTree::new(),
-            theme: Theme::new(),
-            epoch: 0
-        });
-    }
-}
+        let theme = Theme::new();
+        let mut epoch = Epoch(0);
+        let mut dirty = true;
+        let mut busy_rendering = AtomicBool::new(false);
+        let mut foo = true;
 
-impl Window for WebrenderWindow {
-    fn size(&self) -> &DeviceUintSize {
-        &self.size
-    }
-
-    fn run(&mut self) {
-        'outer: loop {
-            let mut events: Vec<glutin::Event> = Vec::new();
-            events.push(self.window.wait_events().next().unwrap());
-
-            for event in self.window.poll_events() {
-                events.push(event);
+        events_loop.run_forever(|event| {
+            match event {
+                glutin::Event::Awakened => {
+                    println!("Awakened");
+                    let _ = gl_window.swap_buffers().unwrap();
+                    *busy_rendering.get_mut() = false;
+                },
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => return glutin::ControlFlow::Break,
+                    glutin::WindowEvent::Resized(w, h) => {
+                        gl_window.resize(w, h);
+                        dirty = true;
+                        foo = true;
+                    }
+                    _ => (),
+                },
+                _ => ()
             }
 
-            for event in events {
-                match event {
-                    glutin::Event::Closed |
-                    glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
-                    glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Q)) => {
-                        break 'outer
+            if dirty {
+                if busy_rendering.compare_and_swap(false, true, Ordering::Relaxed) == false {
+                    println!("Was not busy rendering, so starting now...");
+                    dirty = false;
+
+                    // Stupid workaround to render the first frame twice
+                    if foo {
+                        foo = false;
+                        dirty = true;
                     }
 
-                    glutin::Event::Resized(width, height) => {
-                        self.size = DeviceUintSize::new(width, height);
-                        self.build_tree();
-                    }
+                    let size = gl_window.get_inner_size_pixels().unwrap();
+                    println!("Rendering size: {:?}", size);
 
-                    glutin::Event::KeyboardInput(glutin::ElementState::Pressed,
-                                                 _,
-                                                 Some(glutin::VirtualKeyCode::P)) => {
-                        let enable_profiler = !self.renderer.get_profiler_enabled();
-                        self.renderer.set_profiler_enabled(enable_profiler);
-                        self.api.generate_frame(None);
-                    }
+                    renderer.update();
 
-                    _ => {}//event_handler(&event, &api),
+                    self.build_tree(TreeBuilderContext {
+                        size: DeviceUintSize::new(size.0, size.1),
+                        theme: &theme,
+                        api: &api,
+                        epoch: &epoch
+                    });
+                    epoch.0 = epoch.0 + 1;
+
+                    renderer.render(DeviceUintSize::new(size.0, size.1));
                 }
             }
 
-            self.renderer.update();
-            self.renderer.render(self.size);
-            self.window.swap_buffers().ok();
-        }
+            glutin::ControlFlow::Continue
+        });
     }
 
     fn update_tree(&mut self, tree_builder: &Fn() -> ComponentTree) {
         self.tree = tree_builder();
-        self.build_tree();
     }
 }
 
 impl WebrenderWindow {
-    fn build_tree(&mut self) {
-        let layout_size = LayoutSize::new(self.size.width as f32, self.size.height as f32);
+    fn build_tree(&mut self, context: TreeBuilderContext) {
+        let layout_size = LayoutSize::new(context.size.width as f32, context.size.height as f32);
         let mut builder = DisplayListBuilder::new(PipelineId(0, 0), layout_size);
-        self.theme.build_display_list(&mut builder, &self.tree, &layout_size);
+        context.theme.build_display_list(&mut builder, &self.tree, &layout_size);
 
-        let epoch = Epoch(self.epoch);
-        self.epoch = self.epoch + 1;
         let root_background_color = ColorF::new(0.0, 0.7, 0.0, 1.0);
-        self.api.set_window_parameters(self.size, DeviceUintRect::new(DeviceUintPoint::new(0, 0), self.size));
-        self.api.set_display_list(Some(root_background_color),
-                             epoch,
-                             layout_size,
-                             builder.finalize(),
-                             true);
+        context.api.set_window_parameters(context.size, DeviceUintRect::new(DeviceUintPoint::zero(), context.size));
+        context.api.set_display_list(Some(root_background_color),
+                                  *context.epoch,
+                                  layout_size,
+                                  builder.finalize(),
+                                  true);
 
-        self.api.generate_frame(None);
+        context.api.generate_frame(None);
     }
 }
